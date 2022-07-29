41576ec153b4b48d88ab8185173a8eab13aa8de3
Mihail <Dante9041@mail.ru>
2022-07-29 13:57:06 +0300

    add metod GetData

--- Program.cs
+++ Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string[] GetData(string path)
+{
+    string[] res = File.ReadAllText(path)
+      .Split(new char[] { ' ' }
+      , StringSplitOptions.RemoveEmptyEntries);
+    string[] element = new string[res.Count()];
+    for (int i = 0; i < element.Length; i++)
+        element[i] = res[i];
+    return element;
+}
