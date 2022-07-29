769e9277803221ff056ce15a2ca916c3d278a10d
Mihail <Dante9041@mail.ru>
2022-07-29 14:15:26 +0300

    add metod Print

--- Program.cs
+++ Program.cs
@@ -42,3 +42,15 @@ string[] FunctionSelectedElement(string[] inputArray)
     }
     return outputArray;
 }
+
+string Print(string[] array)
+{
+    int length = array.Length;
+    string output = String.Empty;
+    for (int i = 0; i < length; i++)
+    {
+        if(i==length-1) output += ($"{array[i]}.");
+        else output += ($"{array[i]}, ");
+    }
+    return output;
+}
